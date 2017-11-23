7adff7805676f818343675bb48fb343e0dd8b04b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2017-11-22 19:44:57 -0500

    added typescript typings to the project

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for docker-file-parser 1.0
+
+export interface CommandEntry {
+    name: string;
+    args: string[];
+    lineno: number;
+    raw: string;
+    error?: string;
+}
+
+export interface ParseOptions {
+    includeComments: boolean;
+}
+
+export function parse(
+    contents: string,
+    options?: ParseOptions
+): CommandEntry[];
